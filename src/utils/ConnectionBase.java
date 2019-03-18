3bdd783fb2cd8b30b11153b1a1d7ee0714408205
Firas Mehrez <firas.mehrez@esprit.tn>
2019-03-18 19:58:18 +0100

    initial-commit

---
+++ src/utils/ConnectionBase.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author FirasM
+ */
+public class ConnectionBase {
+
+    private String url = "jdbc:mysql://localhost:3306/ecosystem";
+    private String login = "root";
+    private String pwd = "";
+    private Connection cnx;
+    private static ConnectionBase instance; // Pour éviter plusieurs  connexions
+
+    public ConnectionBase() {
+
+        try {
+            cnx = (Connection) DriverManager.getConnection(url, login, pwd);
+            System.out.println("Connection établie ");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public static ConnectionBase getInstance() {
+        if (instance == null) {
+            instance = new ConnectionBase();
+        }
+        return (instance);
+    }
+
+    public Connection getCnx() {
+        return cnx;
+    }
+
+}
